4837748e0d9003e7008991504905fe5f3ccbf33b
toxirsad_off <toxa.saidoff@gmail.com>
2022-03-11 16:06:02 +0100

    removed console.log

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const fs = require('fs')
 const port = process.env.PORT || 8080
 
 
@@ -10,10 +9,6 @@ app.use(express.static(root));
 app.get("*", (req, res) => {
   res.sendFile('index.html', { root });
 })
-
-
-const folderPath = './dist'
-console.log(fs.readdirSync(folderPath))
 
 app.use('*', express.static(path.join(__dirname, '../dist')))
 
