1b5c29b1486f139a1fa409904561fdaedd485ddd
toxirsad_off <toxa.saidoff@gmail.com>
2022-03-12 12:21:07 +0100

    edited gitignore

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const port = process.env.PORT || 8080
-
+const fs = require("fs")
 
 const root = path.join(__dirname, '../', 'dist')
 app.use(express.static(root));
