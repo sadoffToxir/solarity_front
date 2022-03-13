dbe331b0505814963b3d08760261af7b7bfba510
toxirsad_off <toxa.saidoff@gmail.com>
2022-03-13 12:31:53 +0100

    added port

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ const app = express()
 const path = require('path')
 const fs = require("fs")
 
+const port = process.env.PORT || 8080
+
 const root = path.join(__dirname, '../', 'dist')
 app.use(express.static(root));
 app.get("*", (req, res) => {
